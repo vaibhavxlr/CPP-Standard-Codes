ac44ba09e4a6fc9805db80045c0a43def4812322
Vaibhav Upadhyay <vaibhavxlr@gmail.com>
2021-01-05 15:11:39 +0530

    improved Bear and segment

--- Linear Search/Bear and Segment 01.cpp
+++ Linear Search/Bear and Segment 01.cpp
@@ -4,6 +4,26 @@ using namespace std;
 typedef long long int lli;
 #define mod 1000000007
 #define fLoop for(lli i = 0; i < N; i++)
+
+bool check(string st) {
+        int count = 0;
+        char last = st[0];
+        
+        for(int j = 1; j < st.size(); j++) {
+            if(st[j] != last) {
+                last = st[j];
+                count++;
+            }            
+        }
+        
+        if((count == 0 && st[0] == '0') || count > 2 || (count == 2 && st[0] == '1')) 
+            return false;
+        
+        else
+            return true;
+}
+
+
 main() {
 	ios_base::sync_with_stdio(false);
     cin.tie(NULL);
@@ -15,26 +35,9 @@ main() {
     while(T--){
         string s;
         cin >> s;
-        st.push_back(s);
+        if(check(s)) cout << "YES\n";
+	    else cout << "NO\n";
     }
-    
-    for(int i = 0; i < st.size(); i++) {
-        int count = 0;
-        char last = st[i][0];
-        
-        for(int j = 1; j < st[i].size(); j++) {
-            if(st[i][j] != last) {
-                last = st[i][j];
-                count++;
-            }            
-        }
-        
-        if((count == 0 && st[i][0] == '0') || count > 2 || (count == 2 && st[i][0] == '1')) cout << "NO\n";
-        
-        else
-            cout << "YES\n";
-    }
-  
     
 }
     
